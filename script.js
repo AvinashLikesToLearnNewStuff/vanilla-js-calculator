de581cd7290767288b9765bb4849f27896a4a085
AvinashLikesToLearnNewStuff <106229044+AvinashLikesToLearnNewStuff@users.noreply.github.com>
2023-08-01 18:24:14 +0530

    Apply styling to calculator UI components
    - Set border to none for all elements
    - Add border radius to buttons and container for rounded corners
    - Define grid layout for the calculator container
    - Add spacing between grid items using gap property
    - Style the output field for proper alignment and border

--- script.js
+++ script.js
@@ -1,35 +1,55 @@
 const numbers = document.querySelectorAll("[data-number]");
 const outputField = document.querySelector(".output-field");
 const evaluate = document.querySelector(".equal");
-const clearBtn = document.querySelector(".clear");
+const clearbtn = document.querySelector(".clear");
 
-let userInput = "0";
-let currentOperation = "";
-let evaluatedOperation = "";
-let firstNumber = 0;
-let sum = 0;
+let userInput = 0;
+let currentOperation;
+let evaluatedOperation;
+let firstNumber;
+let sum;
 
 numbers.forEach((number) => number.addEventListener("click", getNumber));
-clearBtn.addEventListener("click", clearEverything);
+clearbtn.addEventListener("click", clearEverything);
 
 const operations = document.querySelectorAll(".operation");
 operations.forEach((operation) => operation.addEventListener("click", getSum));
 
-function getNumber(event) {
-  const number = event.currentTarget.dataset.number;
-  userInput += number;
-  outputField.textContent = userInput;
+function getNumber(number) {
+  if (!currentOperation) {
+    userInput += number.currentTarget.dataset.number; //needs to be corrected
+    userInput.toString();
+    outputField.textContent += number.currentTarget.dataset.number;
+    userInput = parseInt(userInput);
+    return userInput;
+  } else {
+    evaluatedOperation = currentOperation;
+    currentOperation === "";
+    userInput += number.currentTarget.dataset.number; //needs to be corrected
+    userInput.toString();
+    outputField.textContent += number.currentTarget.dataset.number;
+    userInput = parseInt(userInput);
+    return userInput;
+  }
 }
 
-function getSum(event) {
-  currentOperation = event.currentTarget.dataset.operator;
+function getSum(Sum) {
+  currentOperation = Sum.currentTarget.dataset.operator;
   outputField.textContent += currentOperation;
-  firstNumber = parseFloat(userInput);
-  userInput = "";
+  console.log(currentOperation);
+  firstNumber = userInput;
+  userInput = 0;
+  return currentOperation;
 }
+
+evaluate.addEventListener("click", evaluateSum);
 
 function evaluateSum() {
-  userInput = parseFloat(userInput);
+  console.log(`${firstNumber}${evaluatedOperation}${userInput}`);
+  //if the user presses = sign, it should calculate the sum
+  firstNumber = parseInt(firstNumber);
+  userInput = parseInt(userInput);
+
   switch (evaluatedOperation) {
     case "+":
       sum = firstNumber + userInput;
@@ -44,18 +64,18 @@ function evaluateSum() {
       sum = firstNumber / userInput;
       break;
     default:
-      console.log("Unsupported operation");
+      console.log("Invalid operation");
   }
+
   console.log(sum);
   outputField.textContent = sum;
-  userInput = sum.toString();
+  userInput = sum;
+  return sum;
 }
-
 function clearEverything() {
-  userInput = "0";
+  userInput = 0;
   currentOperation = "";
   evaluatedOperation = "";
   firstNumber = 0;
-  sum = 0;
-  outputField.textContent = userInput;
+  outputField.textContent = "";
 }
