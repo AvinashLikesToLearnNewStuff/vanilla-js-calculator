55e07a075cc7a804e9fe996ca97758780fec6b48
AvinashLikesToLearnNewStuff <106229044+AvinashLikesToLearnNewStuff@users.noreply.github.com>
2023-08-01 16:56:49 +0530

    made the evaluate function
    tested it and it works fine with the sum function now im planning to change the operators so that not just the sum function works but all the operators do

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 const numbers = document.querySelectorAll("[data-number]");
 const outputField = document.querySelector(".output-field");
-let userInput;
+const evaluate = document.querySelector(".equal");
+
+let userInput = 0;
 let currentOperation;
+let evaluatedOperation;
 let firstNumber;
+let sum;
 
 numbers.forEach((number) => number.addEventListener("click", getNumber));
+
 const operation = document.querySelector(".operation");
 operation.addEventListener("click", getSum);
 
 function getNumber(number) {
-  while (!currentOperation) {
-    userInput = number.currentTarget.dataset.number; //needs to be corrected
+  if (!currentOperation) {
+    userInput += number.currentTarget.dataset.number; //needs to be corrected
+    userInput.toString();
     outputField.textContent += number.currentTarget.dataset.number;
+    userInput = parseInt(userInput);
+    return userInput;
+  } else {
+    evaluatedOperation = currentOperation;
+    currentOperation === "";
+    userInput += number.currentTarget.dataset.number; //needs to be corrected
+    userInput.toString();
+    outputField.textContent += number.currentTarget.dataset.number;
+    userInput = parseInt(userInput);
     return userInput;
   }
 }
@@ -21,5 +36,18 @@ function getSum(Sum) {
   outputField.textContent += currentOperation;
   console.log(currentOperation);
   firstNumber = userInput;
+  userInput = 0;
   return currentOperation;
+}
+
+evaluate.addEventListener("click", evaluateSum);
+
+function evaluateSum() {
+  console.log(`${firstNumber}+${evaluatedOperation}+${userInput}`);
+  //if the user presses = sign, it should calculate the sum
+  firstNumber = parseInt(firstNumber);
+  userInput = parseInt(firstNumber);
+  evaluatedOperation = parseInt(firstNumber);
+  sum = +firstNumber + evaluatedOperation + +userInput;
+  return sum;
 }
